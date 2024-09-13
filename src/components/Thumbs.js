bd7c180b9e800599c1ef4a9d7ffce2a3441197cf
Syed Abubakar Ali <syedabubakarali@gmail.com>
2024-09-13 16:38:36 +0500

    Created forward and backward functions

--- src/components/Thumbs.js
+++ src/components/Thumbs.js
@@ -1,32 +1,26 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
 
 function Thumbs({ items, currentIndex }) {
-    
-    return (
-        <Fragment>
-            {
-                items.map((catalog, idx) => (
-                    <span   
-                        id={idx} 
-                        key={idx} 
-                        data-testid={'thumb-button-' + idx}
-                    >
-                        <span 
-                            className={'inline-flex w-90 pa-4 image-thumb ' + 
-                                (idx === currentIndex ? 'thumb-selected' : '')} 
-                        >
-                            <span 
-                                className='mx-5 thumb' 
-                                id={idx} 
-                                style={{ backgroundImage: 'url('+ catalog.thumb + ')' }}
-                            />
-                        </span>
-                    </span>
-                ))
+  return (
+    <Fragment>
+      {items.map((catalog, idx) => (
+        <span id={idx} key={idx} data-testid={"thumb-button-" + idx}>
+          <span
+            className={
+              "inline-flex w-90 pa-4 image-thumb " +
+              (idx === currentIndex ? "thumb-selected" : "")
             }
-        </Fragment>
-    )
+          >
+            <span
+              className="mx-5 thumb"
+              id={idx}
+              style={{ backgroundImage: "url(" + catalog.thumb + ")" }}
+            />
+          </span>
+        </span>
+      ))}
+    </Fragment>
+  );
 }
 
-export default Thumbs
-
+export default Thumbs;
